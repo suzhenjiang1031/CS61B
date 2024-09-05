30e5310fa61c3f08204fc3f62aff4e168a12a855
suzhenjiang <suzhenjiang1031@gmail.com>
2024-09-05 17:53:25 +0800

    test

---
+++ lab06/src/BSTMap.java
@@ -0,0 +1,45 @@
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap implements Map61B{
+    @Override
+    public void put(Object key, Object value) {
+
+
+    }
+
+    @Override
+    public Object get(Object key) {
+        return null;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set keySet() {
+        return null;
+    }
+
+    @Override
+    public Object remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+}
